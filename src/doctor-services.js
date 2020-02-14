44df68dbfbfe155e0c05fb71b5e7debff51062cf
Kristina Hengster Tintor <hengster.tintor@gmail.com>
2020-02-14 10:57:35 -0800

    start working on document.ready function and attempt to add async function

--- src/doctor-services.js
+++ src/doctor-services.js
@@ -1,5 +1,5 @@
-export class DoctorServices {
-  async getDoctorServices(medIssue, drName) {
+export class DoctorService {
+  async getDoctorService(medIssue, drName) {
     try {
       let response = await fetch(`https://api.betterdoctor.com/2016-03-01/doctors?location=portland&user_key=API_KEY`);
       let jsonifiedResponse;
--- src/main.js
+++ src/main.js
@@ -1,4 +1,25 @@
-// import $ from 'jquery';
+import { DoctorService } from './doctor-services';
+import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
+
+$(document).ready(function() {
+  $("form").submit(function(event) {
+    event.preventDefault();
+
+    var medIssue = $("input#med-issue").val();
+    var drName = $("input#drName").val();
+
+    (async () => {
+      let doctorService = new DoctorService();
+      const response = await doctorService.getDoctorService(medIssue,drName);
+      getElements();
+    });
+
+    // function getElements(response) {
+    //   if(response)
+    // }
+
+  });
+});
