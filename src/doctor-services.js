7c60d1f5de8dfd0f6444c0e33b0076ff69139e6e
Kristina Hengster Tintor <hengster.tintor@gmail.com>
2020-02-14 10:26:25 -0800

    modify doctor-services.js to include response jsonification

--- src/doctor-services.js
+++ src/doctor-services.js
@@ -2,6 +2,13 @@ export class DoctorServices {
   async getDoctorServices(medIssue, drName) {
     try {
       let response = await fetch(`https://api.betterdoctor.com/2016-03-01/doctors?location=portland&user_key=API_KEY`);
+      let jsonifiedResponse;
+      if(response.ok && response.status === 200) {
+        jsonifiedResponse = await response.json();
+      } else {
+        jsonifiedResponse = false;
+      }
+      return jsonifiedResponse;
     }
     catch (error) {
       return false;
