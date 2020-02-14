412b3e001fd570c4c7039997c48544a43ae0c89c
Kristina Hengster Tintor <hengster.tintor@gmail.com>
2020-02-14 14:53:06 -0800

    finally install dotenv-webpack after multiple unsuccessful file modifications and API calls

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-// import { DoctorService } from './doctor-service';
+// import { DoctorService } from './doctor-service.js';
 import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -11,12 +11,35 @@ $(document).ready(function() {
     var medIssue = $("input#med-issue").val();
     var drName = $("input#dr-name").val();
 
-    $("p#doctors").text(medIssue + " " + drName);
+    console.log(medIssue + " " + drName);
+
+    let request = new XMLHttpRequest();
+    const url = `https://api.betterdoctor.com/2016-03-01/doctors?location=or-portland&user_key=${process.env.API_KEY}`;
+
+    request.onreadystatechange = function() {
+      if (this.readyState === 4 && this.status === 200) {
+        const response = JSON.parse(this.responseText);
+        getElements(response);
+      }
+    };
+
+    request.open("GET", url, true);
+    request.send();
+
+    const getElements = function(response) {
+      $("p#doctors").text(response);
+    };
+
+
+
 
     // (async () => {
     //   let doctorService = new DoctorService();
+    //   console.log(medIssue);
     //   const response = await doctorService.getDoctorService(medIssue,drName);
+      
     //   getElements();
+     
     // });
 
     // function getElements(response) {
@@ -26,6 +49,10 @@ $(document).ready(function() {
     //     $("p#doctors").text("No doctors for you.");
     //   }
     // }
+
+   
+
+
 
   });
 });
