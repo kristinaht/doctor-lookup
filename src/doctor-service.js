d041d252b2ad36a1119f8290a8f1fc0ab3f4f69f
Kristina Hengster Tintor <hengster.tintor@gmail.com>
2020-02-23 17:14:36 -0800

    attempt to implement logic to display multiple doctors

--- src/doctor-service.js
+++ src/doctor-service.js
@@ -2,7 +2,7 @@ export class DoctorService {
   async getDoctorService(medIssue, drName) {
     console.log("hello");
     try {
-      let response = await fetch(`https://api.betterdoctor.com/2016-03-01/doctors?name=${drName}&query=${medIssue}&location=or-portland&skip=0&limit=100&user_key=${process.env.API_KEY}`);
+      let response = await fetch(`https://api.betterdoctor.com/2016-03-01/doctors?name=${drName}&query=${medIssue}&location=or-portland&skip=0&limit=50&user_key=${process.env.API_KEY}`);
       console.log(response);
       let jsonifiedResponse;
       if(response.ok && response.status === 200) {
--- src/main.js
+++ src/main.js
@@ -20,10 +20,42 @@ $(document).ready(function() {
     })();
 
     function getElements(response) {
+     
       for(let i=0; i<response.data.length; i++) {
-        console.log(response.data[i].profile.first_name);
+        console.log(response.data[i].profile.first_name, response.data[i].profile.last_name);
+       
+     
+        $("div#display-response").show();          
+        
+        $("span.fName").text(response.data[i].profile.first_name);
+        $("span.lName").text(response.data[i].profile.last_name);
+
+        for(let index=0; index<response.data[i].practices.length; index++) {
+          console.log(response.data[i].practices[index].visit_address.street);
+          console.log(response.data[i].practices[index].visit_address.zip);
+          console.log(response.data[i].practices[index].visit_address.city);
+          console.log(response.data[i].practices[index].visit_address.state);
+          console.log(response.data[i].practices[index].phones.number);
+
+          $("span.street").text(response.data[i].practices[index].visit_address.street);
+          $("span.city").text(response.data[i].practices[index].visit_address.city);
+          $("span.state").text(response.data[i].practices[index].visit_address.state);
+          $("span.zip").text(response.data[i].practices[index].visit_address.zip);
+          $("span.phone").text(response.data[i].practices[index].phones.number);
+          $("span.website").text(response.data[i].practices[index].phones[index].number);
+          
+
+          if(response.data[i].practices[index].accepts_new_patients == true) {
+           console.log("accepts new pts");
+           $("p.newPt").text("Yes");
+          }else{
+             console.log("doesn't accept new pts");
+             $("p.newPt").text("No");
+           }   
+        
+          }
+
       }
     }
-
   });
 });
