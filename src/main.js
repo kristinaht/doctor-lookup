585716ceccde9f286ac43df660cadf66369dbd5f
Kristina Hengster Tintor <hengster.tintor@gmail.com>
2020-02-24 20:46:41 -0800

    successfully add logic to display all doctor names from result

--- src/main.js
+++ src/main.js
@@ -8,45 +8,43 @@ $(document).ready(function() {
   $("form").submit(function(event) {
     event.preventDefault();
 
-    var query = $("input#med-issue").val();
+   let query = $("#query").val();
+   
+
 
     (async () => {
       let doctorService = new DoctorService();
-      const response = await doctorService.getDoctorService(drName, medIssue);
+      const response = await doctorService.getDoctorService(query);
       getElements(response);
     })();
 
     function getElements(response) {
-     
-      for(let i=0; i<response.data.length; i++) {
-        console.log(response.data[i].profile.first_name, response.data[i].profile.last_name);
-       
-     
-        $("div#display-response").show();          
+        $("div#display-response").show();  
+        let output = "";
+        for(let i=0; i<response.data.length; i++) {
+        output += `<p>${response.data[i].profile.first_name} ${response.data[i].profile.last_name}</p>`;
+        $("div#display-response").html(output);
+
         
-        // response.data.forEach(function(response) {
-        //   $("p.drName").prepend('<span>'(response.data[i].profile.first_name)`</span>`);
-        // });
-
-        // $("#display-response").append(`${response.data[i].profile.first_name}`);
-        $("span.fName").append(`${response.data[i].profile.first_name} ${response.data[i].profile.last_name}<br>`);
-        // $("span.lName").append(`<br>`);
-
        
+      
+         
+        // $("span.fName").text(response.data[i].profile.first_name);
+        // $("span.lName").text(response.data[i].profile.last_name);
 
         for(let index=0; index<response.data[i].practices.length; index++) {
           console.log(response.data[i].practices[index].visit_address.street);
           console.log(response.data[i].practices[index].visit_address.zip);
           console.log(response.data[i].practices[index].visit_address.city);
           console.log(response.data[i].practices[index].visit_address.state);
-          console.log(response.data[i].practices[index].phones.number);
+        
 
           $("span.street").text(response.data[i].practices[index].visit_address.street);
           $("span.city").text(response.data[i].practices[index].visit_address.city);
           $("span.state").text(response.data[i].practices[index].visit_address.state);
           $("span.zip").text(response.data[i].practices[index].visit_address.zip);
           $("span.phone").text(response.data[i].practices[index].phones.number);
-          // $("span.website").text(response.data[i].practices[index].phones[index].number);
+          
           
 
           if(response.data[i].practices[index].accepts_new_patients == true) {
